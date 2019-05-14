cd242f36b7020e0294ed2aa3eb06a6473b5be238
阿星Plus <hackxing@foxmail.com>
2019-05-14 18:07:49 +0800

    Plus Starter

---
+++ src/Plus/PlusStarter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plus
+{
+    public class PlusStarter : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
