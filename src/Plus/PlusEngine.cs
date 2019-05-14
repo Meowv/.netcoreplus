8c0fd26c5571f542cc55d055fe6ba71915146125
阿星Plus <hackxing@foxmail.com>
2019-05-14 17:55:24 +0800

    Plus Engine

---
+++ src/Plus/PlusEngine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plus
+{
+    public class PlusEngine
+    {
+        private readonly bool _initialized = false;
+    }
+}
