90d4996abf53830c1d466c76acbeb12831609d07
阿星Plus <hackxing@foxmail.com>
2019-05-14 21:16:06 +0800

    PlusCoreInstaller

---
+++ src/Plus/Dependency/Installers/PlusCoreInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Plus.Dependency.Installers
+{
+    internal class PlusCoreInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                // TODO...
+            );
+        }
+    }
+}
