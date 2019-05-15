e5648bfba527aee88461fb2bc4bced62c9b92b98
阿星Plus <hackxing@foxmail.com>
2019-05-15 13:23:56 +0800

    UnitOfWork

---
+++ src/Plus/Domain/Uow/IActiveUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plus.Domain.Uow
+{
+    public interface IActiveUnitOfWork
+    {
+    }
+}
---
+++ src/Plus/Domain/Uow/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plus.Domain.Uow
+{
+    public interface IUnitOfWork : IActiveUnitOfWork, IUnitOfWorkCompleteHandle
+    {
+
+    }
+}
