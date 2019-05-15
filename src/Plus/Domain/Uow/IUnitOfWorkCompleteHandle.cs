8455bd0bb163601b15f091cbf8301f46dc7843c4
阿星Plus <hackxing@foxmail.com>
2019-05-15 14:15:51 +0800

    工作单元

--- src/Plus/Domain/Uow/IUnitOfWorkCompleteHandle.cs
+++ src/Plus/Domain/Uow/IUnitOfWorkCompleteHandle.cs
@@ -1,10 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace Plus.Domain.Uow
 {
-    public interface IUnitOfWorkCompleteHandle
+    /// <summary>
+    /// Used to complete a unit of work.
+    /// This interface can not be injected or directly used.
+    /// Use <see cref="IUnitOfWorkManager"/> instead.
+    /// </summary>
+    public interface IUnitOfWorkCompleteHandle : IDisposable
     {
+        /// <summary>
+        /// Completes this unit of work.
+        /// It saves all changes and commit transaction if exists.
+        /// </summary>
+        void Complete();
+
+        /// <summary>
+        /// Completes this unit of work.
+        /// It saves all changes and commit transaction if exists.
+        /// </summary>
+        Task CompleteAsync();
     }
 }
