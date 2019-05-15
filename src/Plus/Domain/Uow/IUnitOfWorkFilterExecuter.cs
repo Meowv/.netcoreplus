e01ef4cbad7eb696f814a0f2eb434ed1e0964e53
阿星Plus <hackxing@foxmail.com>
2019-05-15 16:32:01 +0800

    🤩🤩🤩

---
+++ src/Plus/Domain/Uow/IUnitOfWorkFilterExecuter.cs
@@ -0,0 +1,11 @@
+﻿namespace Plus.Domain.Uow
+{
+    public interface IUnitOfWorkFilterExecuter
+    {
+        void ApplyDisableFilter(IUnitOfWork unitOfWork, string filterName);
+
+        void ApplyEnableFilter(IUnitOfWork unitOfWork, string filterName);
+
+        void ApplyFilterParameterValue(IUnitOfWork unitOfWork, string filterName, string parameterName, object value);
+    }
+}
---
+++ src/Plus/Domain/Uow/InnerUnitOfWorkCompleteHandle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Plus.Domain.Uow
+{
+    /// <summary>
+    /// This handle is used for innet unit of work scopes.
+    /// A inner unit of work scope actually uses outer unit of work scope
+    /// and has no effect on <see cref="IUnitOfWorkCompleteHandle.Complete"/> call.
+    /// But if it's not called, an exception is thrown at end of the UOW to rollback the UOW.
+    /// </summary>
+    internal class InnerUnitOfWorkCompleteHandle : IUnitOfWorkCompleteHandle, IDisposable
+    {
+        public const string DidNotCallCompleteMethodExceptionMessage = "未调用完整方法的工作单元";
+
+        private volatile bool _isCompleteCalled;
+        private volatile bool _isDisposed;
+
+        public void Complete()
+        {
+            _isCompleteCalled = true;
+        }
+
+        public Task CompleteAsync()
+        {
+            _isCompleteCalled = true;
+            return Task.FromResult(0);
+        }
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            _isDisposed = true;
+
+            if (!_isCompleteCalled)
+            {
+                if (HasException())
+                {
+                    return;
+                }
+
+                throw new PlusException(DidNotCallCompleteMethodExceptionMessage);
+            }
+        }
+
+        private static bool HasException()
+        {
+            try
+            {
+                return Marshal.GetExceptionCode() != 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
