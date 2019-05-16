1a86b42ba20997ea7b93a6c28de9a2e0fd9c1d70
阿星Plus <hackxing@foxmail.com>
2019-05-16 22:02:40 +0800

    Enum Alias Attribute ✔✔✔

---
+++ src/Plus/CodeAnnotations/EnumAliasAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Plus.CodeAnnotations
+{
+    /// <summary>
+    /// 枚举属性，定义别名
+    /// </summary>
+    public class EnumAliasAttribute : Attribute
+    {
+        public string Alias { get; set; }
+
+        public EnumAliasAttribute(string alias)
+        {
+            Alias = alias;
+        }
+    }
+}
---
+++ src/Plus/CodeAnnotations/EnumExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace Plus.CodeAnnotations
+{
+    /// <summary>
+    /// 枚举扩展
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// 获取枚举别名
+        /// </summary>
+        /// <param name="_enum"></param>
+        /// <returns></returns>
+        public static string ToAlias(this Enum _enum)
+        {
+            Type type = _enum.GetType();
+            FieldInfo field = type.GetField(_enum.ToString());
+            if (field == null)
+            {
+                return string.Empty;
+            }
+
+            string result = string.Empty;
+
+            object[] customAttributes = field.GetCustomAttributes(typeof(EnumAliasAttribute), inherit: false);
+            object[] array = customAttributes;
+            for (int i = 0; i < array.Length; i++)
+            {
+                EnumAliasAttribute enumAliasAttribute = (EnumAliasAttribute)array[i];
+                result = enumAliasAttribute.Alias;
+            }
+            return result;
+        }
+    }
+}
