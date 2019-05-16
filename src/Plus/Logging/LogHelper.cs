d24fe49b1885310f79f9d09a3fe1f05e3222c51d
阿星Plus <hackxing@foxmail.com>
2019-05-16 21:55:53 +0800

    LogHelper

---
+++ src/Plus/Logging/LogHelper.cs
@@ -0,0 +1,69 @@
+﻿using Castle.Core.Logging;
+using Plus.Dependency;
+using Plus.Runtime.Validation;
+using System;
+using System.Linq;
+
+namespace Plus.Logging
+{
+    public static class LogHelper
+    {
+        public static ILogger Logger { get; private set; }
+
+        static LogHelper()
+        {
+            Logger = IocManager.Instance.IsRegistered(typeof(ILoggerFactory))
+                ? IocManager.Instance.Resolve<ILoggerFactory>().Create(typeof(LogHelper))
+                : NullLogger.Instance;
+        }
+
+        public static void LogException(Exception ex)
+        {
+            LogException(Logger, ex);
+        }
+
+        public static void LogException(ILogger logger, Exception ex)
+        {
+            LogSeverity severity = (ex as IHasLogSeverity)?.Severity ?? LogSeverity.Error;
+            logger.Log(severity, ex.Message, ex);
+
+            LogValidationErrors(logger, ex);
+        }
+
+        private static void LogValidationErrors(ILogger logger, Exception exception)
+        {
+            //Try to find inner validation exception
+            if (exception is AggregateException && exception.InnerException != null)
+            {
+                var aggException = exception as AggregateException;
+                if (aggException.InnerException is PlusValidationException)
+                {
+                    exception = aggException.InnerException;
+                }
+            }
+
+            if (!(exception is PlusValidationException))
+            {
+                return;
+            }
+
+            var validationException = exception as PlusValidationException;
+            if (validationException.ValidationErrors.IsNullOrEmpty())
+            {
+                return;
+            }
+
+            logger.Log(validationException.Severity, "There are " + validationException.ValidationErrors.Count + " validation errors:");
+            foreach (var validationResult in validationException.ValidationErrors)
+            {
+                var memberNames = "";
+                if (validationResult.MemberNames != null && validationResult.MemberNames.Any())
+                {
+                    memberNames = " (" + string.Join(", ", validationResult.MemberNames) + ")";
+                }
+
+                logger.Log(validationException.Severity, validationResult.ErrorMessage + memberNames);
+            }
+        }
+    }
+}
