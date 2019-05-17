1abcfbfe6d7710773feb112f1538bd31e2338d07
阿星Plus <hackxing@foxmail.com>
2019-05-17 21:47:16 +0800

    Orm Interface

---
+++ src/Plus/Orm/ISecondaryOrmRegistrar.cs
@@ -0,0 +1,15 @@
+﻿using Plus.Dependency;
+using Plus.Domain.Repositories;
+
+namespace Plus.Orm
+{
+    /// <summary>
+    /// ISecondaryOrmRegistrar
+    /// </summary>
+    public interface ISecondaryOrmRegistrar
+    {
+        string OrmContextKey { get; }
+
+        void RegisterRepositories(IIocManager iocManager, AutoRepositoryTypesAttribute defaultRepositoryTypes);
+    }
+}
