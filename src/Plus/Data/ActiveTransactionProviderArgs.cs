904fdb8c9c2ae5a71afef7728af743f92f8024cd
阿星Plus <hackxing@foxmail.com>
2019-05-17 20:56:25 +0800

    IActiveTransactionProvider

---
+++ src/Plus/Data/ActiveTransactionProviderArgs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Plus.Data
+{
+    /// <summary>
+    /// ActiveTransactionProviderArgs
+    /// </summary>
+    public class ActiveTransactionProviderArgs : Dictionary<string, object>
+    {
+        public static ActiveTransactionProviderArgs Empty { get; } = new ActiveTransactionProviderArgs();
+    }
+}
