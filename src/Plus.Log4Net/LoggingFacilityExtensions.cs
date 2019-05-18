02b7f3a34b8f249f867020229d8b1751316cc4a2
阿星Plus <hackxing@foxmail.com>
2019-05-18 22:32:30 +0800

    Log4Net

---
+++ src/Plus.Log4Net/LoggingFacilityExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Castle.Facilities.Logging;
+
+namespace Plus.Log4Net
+{
+    /// <summary>
+    /// LoggingFacilityExtensions
+    /// </summary>
+    public static class LoggingFacilityExtensions
+    {
+        public static LoggingFacility UseLog4Net(this LoggingFacility loggingFacility)
+        {
+            return loggingFacility.LogUsing<Log4NetLoggerFactory>();
+        }
+    }
+}
---
+++ src/Plus.Log4Net/PlusCastleLog4NetModule.cs
@@ -0,0 +1,13 @@
+﻿using Plus.Modules;
+
+namespace Plus.Log4Net
+{
+    /// <summary>
+    /// Castle Log4Net module
+    /// </summary>
+    [DependsOn(typeof(PlusLeadershipModule))]
+    public class PlusCastleLog4NetModule
+    {
+
+    }
+}
