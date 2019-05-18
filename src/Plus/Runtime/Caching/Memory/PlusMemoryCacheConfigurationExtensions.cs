b52d58642aae689f7711e2f05a53bf59e5ebb2a3
阿星Plus <hackxing@foxmail.com>
2019-05-18 14:22:11 +0800

    PlusMemoryCacheConfigurationExtensions

---
+++ src/Plus/Runtime/Caching/Memory/PlusMemoryCacheConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Plus.Dependency;
+using Plus.Runtime.Caching.Configuration;
+
+namespace Plus.Runtime.Caching.Memory
+{
+    public static class PlusMemoryCacheConfigurationExtensions
+    {
+        public static void UseMemoryCache(this ICachingConfiguration cachingConfiguration)
+        {
+            IIocManager iocManager = cachingConfiguration.PlusConfiguration.IocManager;
+            iocManager.RegisterIfNot<ICacheManager, PlusMemoryCacheManager>();
+        }
+    }
+}
